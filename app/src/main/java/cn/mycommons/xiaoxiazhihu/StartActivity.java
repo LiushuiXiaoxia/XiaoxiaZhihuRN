e99811f37980e34c7547aefa79611ceca320ffe1
xiaqiulei <qiulei.xia@ele.me>
2016-05-04 10:25:03 +0800

    init

---
+++ app/src/main/java/cn/mycommons/xiaoxiazhihu/StartActivity.java
@@ -0,0 +1,13 @@
+package cn.mycommons.xiaoxiazhihu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
